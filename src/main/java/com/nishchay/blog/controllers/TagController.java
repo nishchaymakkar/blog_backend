fce3673296c01f8d4675d5a4e8c0e6b169b64c22
Nishchay Makkar <nishchaymakkar5@gmail.com>
2025-02-24 22:36:06 +0530

    -issue tag controller

--- src/main/java/com/nishchay/blog/controllers/TagController.java
+++ src/main/java/com/nishchay/blog/controllers/TagController.java
@@ -24,8 +24,6 @@ public class TagController {
     @GetMapping
     public ResponseEntity<List<TagResponse>> getAllTags(){
         List<Tag> tags = tagService.getTags();
-
-        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
     }
 }
